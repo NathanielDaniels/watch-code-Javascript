86bc61c0e6161dd9dacc70c5a93cbb967e6f4c0e
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-08-08 19:47:44 -0500

    dealing with some linger issues

--- node.js/script.js
+++ node.js/script.js
@@ -1,17 +1,23 @@
 let http = require("http"); //access to HTTP module (to create server)
-let dt = require("./myfirstmodule");
+// let dt = require("./myfirstmodule");
+let fs = require("fs");
 
 http
   .createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    //* Homepage
-    if (req.url == "/") {
-      res.write(`Hello, and Welcome to our Homepage ${dt.myDateTime()}`);
+    // res.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("main.html", function(err, data) {
+      res.writeHead(200, { "content-Type": "text/html" });
+      res.write(data);
       res.end();
-    }
+    });
+    //* Homepage
+    // if (req.url == "/") {
+    //   res.write(`Hello, and Welcome to our Homepage ${dt.myDateTime()}`);
+    //   res.end();
+    // }
     //* About
-    if (req.url == "/about") {
-      res.end("About Page");
-    }
+    // if (req.url == "/about") {
+    //   res.end("About Page");
+    // }
   })
   .listen(3000);
